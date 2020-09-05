ea7427d2b5dd068ee57ae44cf58d06c1a297581e
Nitin <48169425+nitinsharmacs@users.noreply.github.com>
2020-09-05 11:22:06 +0530

    Update sendMail.js

--- util/sendMail.js
+++ util/sendMail.js
@@ -3,8 +3,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
 	service:'gmail',
 	auth:{
-		user:'rahuljhasharma123@gmail.com',
-		pass:'nitin123@#'
+		user:process.env.EMAIL_USER,
+		pass:process.env.EMAIL_PASSWORD
 	}
 });
 
