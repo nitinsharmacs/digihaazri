296d33d7194c986ebaeb0c1a7b4ce103d2f80300
Nitin <48169425+nitinsharmacs@users.noreply.github.com>
2023-10-20 23:53:50 +0530

    remove db secret

--- util/dbconnection.js
+++ util/dbconnection.js
@@ -1,7 +1,7 @@
 const mongodb  = require('mongodb');	//importing mongodb
 const MongoClient = mongodb.MongoClient;		//making a MongoClient variable
 const URL1 = 'mongodb://127.0.0.1:27017';
-const URL = process.env.DB_URL || 'mongodb+srv://nitin:nitin123@cluster0-jx9oe.mongodb.net?retryWrites=true&w=majority';
+const URL = process.env.DB_URL;
 const DB_NAME = process.env.DB_NAME || 'expressJs';
 const dbconnection = cb => {
 	MongoClient.connect(URL, {useNewUrlParser: true}).then(client=>{
