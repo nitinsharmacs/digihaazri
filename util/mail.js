488c440ea45e1cbaf773b8016690f4702149a19b
Nitin <48169425+nitinsharmacs@users.noreply.github.com>
2023-10-20 23:55:33 +0530

    Update mail.js

--- util/mail.js
+++ util/mail.js
@@ -1,5 +1,5 @@
 const  sgMail = require('@sendgrid/mail');
-sgMail.setApiKey('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');	
+sgMail.setApiKey('');	
 
 const mail = () => {
 	return sgMail;
